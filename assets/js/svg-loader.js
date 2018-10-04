840d68fc59e40698bc7ed446cca142d9bde44b67
Zweih <fernandon299@gmail.com>
2018-10-04 12:42:32 -0400

    Initial commit.

---
+++ assets/js/svg-loader.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+  const spans = $("span.svg-icon");
+
+  for(let i = 0; i < spans.length; i++) {
+    const span = spans.get(i);
+    const classArr = span.className.split(/\s+/);
+    const svgUrl = `./assets/svg/${classArr[1]}/${classArr[2]}.svg`;
+    const title = $(`h3.${classArr[2]}`);
+    // $(span).load(`./assets/svg/${classArr[1]}/${classArr[2]}.svg`)
+
+    $.get(svgUrl, (data) => {
+      const docEl = data.documentElement;
+      const svg = new XMLSerializer().serializeToString(docEl);
+      $(span).prepend(svg);
+     }
+    );
+  }
+});
